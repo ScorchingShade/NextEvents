7eb33ca363866802455879b67fb25bbf03e955c1
ScorchingShade <ankushors789@gmail.com>
2021-09-18 01:23:27 +0530

    added programatic navigation, corrected url error

--- pages/events/index.js
+++ pages/events/index.js
@@ -9,7 +9,7 @@ function AllEventsPage(props) {
   const router = useRouter();
 
   function findEventsHandler(year, month) {
-      const fullPath=`/events${year}/${month}`
+      const fullPath=`/events/${year}/${month}`
     
     router.push(fullPath);
   }
