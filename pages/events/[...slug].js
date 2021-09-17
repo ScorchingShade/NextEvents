cdd4b54a4f273d23284cfe114211aa540c0e8469
ScorchingShade <ankushors789@gmail.com>
2021-09-18 01:32:12 +0530

    added filters to check filtered events

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from "react";
+import { useRouter } from "next/router";
+import { getFilteredEvents } from "../../dummy-data";
 
 function FilteredEventsPage() {
-    return (
-        <div>
-            <h1>Filtered Events</h1>
-        </div>
-    )
+  const router = useRouter();
+
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className="center">Loading....</p>;
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid filter. Please adjust your values!</p>;
+  }
+
+  const filteredEvents=getFilteredEvents(numYear, numMonth);
+
+  if(!filterData || filteredEvents.length===0){
+      <p>No events found for chosen filter</p>
+  }
+
+  return (
+    <div>
+      <h1>Filtered Events</h1>
+    </div>
+  );
 }
 
-export default FilteredEventsPage
+export default FilteredEventsPage;
