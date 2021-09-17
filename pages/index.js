519f3d1ff020c11e9dc5c6b21a2c15b5d0a09bd8
ScorchingShade <ankushors789@gmail.com>
2021-09-17 15:25:47 +0530

    created project structure to represent end file path

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import React from 'react'
 
-export default function Home() {
+function HomePage() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Homepage</h1>
     </div>
   )
 }
+
+export default HomePage
