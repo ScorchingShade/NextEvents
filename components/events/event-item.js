a0325e2450d7ba2fa02b9ba3c718742ce1998ca5
ScorchingShade <ankushors789@gmail.com>
2021-09-17 21:37:07 +0530

    added structure for events and files

---
+++ components/events/event-item.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Link from 'next/link'
+
+function EventItem(props) {
+
+    const {title, image, date, location, id}=props;
+    const humanReadableDate=new Date(date).toLocaleDateString('en-US',{
+        day:"numeric",
+        month:'long',
+        year:"numeric"
+    });
+
+    
+    const formattedAddress = location.replace(',','\n');
+    const exploreLink=`/events/${id}`
+
+  return (
+    <li>
+      <img src={`/${image}`} alt={title} />
+      <div>
+        <div>
+          <div>
+            <h2>Title</h2>
+          </div>
+          <div>
+            <time>{humanReadableDate}</time>
+          </div>
+          <div>
+            <address>{formattedAddress}</address>
+          </div>
+        </div>
+        <div>
+            <Link href={exploreLink}>Explore Event</Link>
+        </div>
+      </div>
+      
+    </li>
+  );
+}
+
+export default EventItem;
