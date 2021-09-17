61d45ef7bd2e9111637a88ccdbb5254476472e64
ScorchingShade <ankushors789@gmail.com>
2021-09-18 02:12:11 +0530

    added error component, completed first leg of development

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -3,6 +3,8 @@ import { useRouter } from "next/router";
 import { getFilteredEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
 import ResultsTitle from "../../components/events/results-title";
+import Button from "../../components/ui/button";
+import ErrorAlert from "../../components/ui/error-alert";
 
 function FilteredEventsPage() {
   const router = useRouter();
@@ -26,26 +28,45 @@ function FilteredEventsPage() {
     numMonth < 1 ||
     numMonth > 12
   ) {
-    return <p>Invalid filter. Please adjust your values!</p>;
+    return (
+        <Fragment>
+        <ErrorAlert>
+          <p>Invalid filter. Please adjust your values!</p>
+        </ErrorAlert>
+        <br/>
+        <div className='center'>
+          <Button link='/events'>Show All Events</Button>
+        </div>
+      </Fragment>
+    );
   }
 
   const filteredEvents = getFilteredEvents({
     year: numYear,
     month: numMonth,
   });
-  
 
-  if(!filteredEvents || filteredEvents.length===0){
-     return <p>No events found for chosen filter</p>
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+        <Fragment>
+        <ErrorAlert>
+          <p>No events found for the chosen filter!</p>
+        </ErrorAlert>
+        <br/>
+        <div className='center'>
+          <Button link='/events'>Show All Events</Button>
+        </div>
+      </Fragment>
+    );
   }
 
-  const date = new Date(numYear, numMonth -1);
+  const date = new Date(numYear, numMonth - 1);
 
   return (
     <Fragment>
-        <ResultsTitle date={date}/>
-      <EventList items={filteredEvents}/>
-      </Fragment>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </Fragment>
   );
 }
 
