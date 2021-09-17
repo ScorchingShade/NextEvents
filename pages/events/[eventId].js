3411c1c7e6807f0cdfbdd4ed16b9ea3006b8b27c
ScorchingShade <ankushors789@gmail.com>
2021-09-17 15:26:01 +0530

    added all files

---
+++ pages/events/[eventId].js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function EventDetailPage() {
+    return (
+        <div>
+            <h1>EventDetail</h1>
+        </div>
+    )
+}
+
+export default EventDetailPage
---
+++ pages/events/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AllEventsPage() {
+    return (
+        <div>
+            <h1>All Events</h1>
+        </div>
+    )
+}
+
+export default AllEventsPage
